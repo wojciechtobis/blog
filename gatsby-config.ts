f96f01c90555d69a960e33e832ae3a3f01581c8e
wojciechtobis <wojciechtobis@github.com>
2022-04-09 21:35:45 +0200

    Simplify main and 404 pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
+  pathPrefix: "/Blog",
   siteMetadata: {
     title: `WT's blog`,
     siteUrl: `https://www.yourdomain.tld`,
